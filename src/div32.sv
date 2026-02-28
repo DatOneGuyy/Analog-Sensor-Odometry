09fdb097dd74031af001c6cdcca1d954f4ef1592
DatOneGuyy <computingpotatochips@gmail.com>
2026-02-28 04:46:05 -0800

    basic implementation without special cases and buggy rounding

--- src/div32.sv
+++ src/div32.sv
@@ -10,10 +10,85 @@ assign b = 32'hc0ba2e8c;
 assign c = 32'h4087c1f0;
 
 fp32_t shifted_divisor, shifted_dividend;
-assign shifted_divisor = {divisor.sign, 8'd126, divisor.mantissa};
+logic signed [7:0] shift = 8'sd126 - divisor.exponent;
 
-logic signed [7:0] shift = 8'd126 - shifted_divisor.exponent;
-assign shifted_dividend = {dividend.sign, dividend.exponent + shift, dividend.mantissa};
+assign shifted_divisor = {1'b0, 8'd126, divisor.mantissa};
+
+assign shifted_dividend = {1'b0, dividend.exponent + shift, dividend.mantissa};
+
+logic divisor_LSB, dividend_LSB, round_up_divisor, round_up_dividend;
+logic [2:0] divisor_GRS, dividend_GRS;
+logic [25:0] shifted_divisor_mantissa, shifted_dividend_mantissa;
+
+fp32_t rounded_divisor, rounded_dividend;
+logic [25:0] temp_divisor_mantissa, temp_dividend_mantissa;
+
+assign round_up_divisor = divisor_GRS[2] & (divisor_GRS[1] | divisor_GRS[0] | divisor_LSB);
+assign round_up_dividend = dividend_GRS[2] & (dividend_GRS[1] | dividend_GRS[0] | dividend_LSB);
+always_comb begin : rounding
+    if (shift < 0 & shift > -26) begin
+        shifted_divisor_mantissa = {1'b1, divisor.mantissa, 2'b0} >> shift;
+        shifted_dividend_mantissa = {1'b1, dividend.mantissa, 2'b0} >> shift;
+
+        divisor_LSB = shifted_divisor_mantissa[2];
+        dividend_LSB = shifted_dividend_mantissa[2];
+
+        divisor_GRS = {shifted_divisor_mantissa[1:0], |(divisor.mantissa << (26 + shift))};
+        dividend_GRS = {shifted_dividend_mantissa[1:0], |(dividend.mantissa << (26 + shift))};
+    end
+    else if (shift <= -26) begin
+        shifted_divisor_mantissa = 26'b0;
+        shifted_dividend_mantissa = 26'b0;
+
+        divisor_LSB = 1'b0;
+        dividend_LSB = 1'b0;
+
+        divisor_GRS = {2'b0, |divisor.mantissa};
+        dividend_GRS = {2'b0, |dividend.mantissa};
+    end
+    else begin
+        shifted_divisor_mantissa = {1'b1, divisor.mantissa, 2'b0};
+        shifted_dividend_mantissa = {1'b1, dividend.mantissa, 2'b0};
+
+        divisor_LSB = 1'b0;
+        dividend_LSB = 1'b0;
+
+        divisor_GRS = 3'b0;
+        dividend_GRS = 3'b0;
+    end
+
+    if (round_up_divisor) begin
+        rounded_divisor.sign = shifted_divisor.sign;
+        temp_divisor_mantissa = shifted_divisor_mantissa + 26'h0000004;
+        rounded_divisor.mantissa = temp_divisor_mantissa[25:3];
+
+        if (temp_divisor_mantissa[25:2] == 24'b0) begin
+            rounded_divisor.exponent = shifted_divisor.exponent + 1;
+        end
+        else begin
+            rounded_divisor.exponent = shifted_divisor.exponent;
+        end
+    end
+    else begin
+        rounded_divisor = shifted_divisor;
+    end
+
+    if (round_up_dividend) begin
+        rounded_dividend.sign = shifted_dividend.sign;
+        temp_dividend_mantissa = shifted_dividend_mantissa + 26'h0000004;
+        rounded_dividend.mantissa = (temp_dividend_mantissa[25:3]);
+
+        if (temp_dividend_mantissa[25:2] == 24'b0) begin
+            rounded_dividend.exponent = shifted_dividend.exponent + 1;
+        end
+        else begin
+            rounded_dividend.exponent = shifted_dividend.exponent;
+        end
+    end
+    else begin
+        rounded_dividend = shifted_dividend;
+    end
+end
 
 fp32_t linear_term;
 mult32 linear(
@@ -39,7 +114,7 @@ mult32 square(
 fp32_t quadratic_term;
 mult32 quadratic(
     .a(divisor_square),
-    .b(c),
+    .b(a),
     .product(quadratic_term)
 );
 
@@ -64,7 +139,20 @@ nr_iterate iteration2(
     .next_X(third_estimate)
 );
 
-assign quotient = shifted_dividend * third_estimate;
+fp32_t final_product;
+mult32 final_multiplier(
+    .a(third_estimate),
+    .b(shifted_dividend),
+    .product(final_product)
+);
+
+logic signed [7:0] test;
+always_comb begin
+    test = shift;
+    //$display("shift: %b", test);
+end
+
+assign quotient = {divisor[31] ^ dividend[31], final_product.exponent, final_product.mantissa};
 
 endmodule
 
